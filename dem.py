83199e4900e7e69a8b0bf828dfb91b6546a2d61d
mine820 <mine820@infoseek.jp>
2020-10-04 12:42:56 +0900

    DEM読み込みを追加

---
+++ dem.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+import numpy as np
+
+class DEM:
+  def __init__(self, filename):
+    # 国土地理院のDEMデータ（5mメッシュ）を読み込む
+    with open(filename, encoding='UTF-8') as f:
+      self.soup = BeautifulSoup(f, 'xml')
+
+    # 緯度経度の最小値／最大値の取得
+    for e in self.soup.find_all('gml:lowerCorner'):
+      self.low_latitude = float(e.contents[0].split()[0])
+      self.low_longitude = float(e.contents[0].split()[1])
+    for e in self.soup.find_all('gml:upperCorner'):
+      self.upper_latitude = float(e.contents[0].split()[0])
+      self.upper_longitude = float(e.contents[0].split()[1])
+
+    # データ数（横、縦）の取得
+    for e in self.soup.find_all('gml:low'):
+       low_x = int(e.contents[0].split()[0])
+       low_y = int(e.contents[0].split()[1])
+    for e in self.soup.find_all('gml:high'):
+       high_x = int(e.contents[0].split()[0])
+       high_y = int(e.contents[0].split()[1])
+    self.x = high_x - low_x + 1
+    self.y = high_y - low_y + 1
+
+    # 標高データの取得
+    depth = []
+    for e in self.soup.find_all('gml:tupleList'):
+      for d in e.contents[0].split('\n'):
+        if (len(d.split(',')) < 2):
+          continue
+        depth.append(float(d.split(',')[1]))
+    self.depthArray = np.array(depth)
+    self.depthArray = np.reshape(self.depthArray, (self.x, self.y))
+
+  # 緯度経度の最小値／最大値の取得
+  def GetArea(self):
+    return self.upper_latitude, self.upper_longitude, self.low_latitude, self.low_longitude
+
+  # データ数（横、縦）の取得
+  def GetSize(self):
+    return self.x, self.y
+
+  # 標高データの取得
+  def GetDepth(self):
+    return self.depthArray
---
+++ test2.py
@@ -0,0 +1,17 @@
+from dem import DEM
+
+filename = 'FG-GML-5640-55-00-DEM5A-20161001.xml'
+
+if __name__ == '__main__':
+  d = DEM(filename)
+
+  ulat, ulong, llat, llong = d.GetArea()
+  print(ulat, ulong, llat, llong)
+
+  x, y = d.GetSize()
+  print(x, y)
+
+  depthArray = d.GetDepth()
+  for yy in range(y):
+    for xx in range(x):
+      print(depthArray[xx,yy])
